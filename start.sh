3a0ae832011f90ae0bfe3c39b5bfb8e8bcb7622b
fanshaohua.fan <fanshaohua.fan@gmail.com>
2015-10-28 00:50:22 +0800

    initail wxserver

---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run --name flask -d -p 5000:5000 -v ./webapp:/app fanshaohua.fan/flask python web.py
---
+++ webapp/web.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
