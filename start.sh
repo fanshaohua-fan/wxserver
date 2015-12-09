d5779049f0384e9b3ea425518aad280c3d0b7985
fanshaohua.fan <fanshaohua.fan@gmail.com>
2015-12-09 21:16:46 +0800

    modify the port of nginx

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker run --name flask -d -v ~/sourcecode/wxserver/webapp/:/app fanshaohua.fan/flask python web.py
-docker run --name wx -p 80:80 --link flask -d fanshaohua.fan/nginx
+docker run --name wx -p 80:8080 --link flask -d fanshaohua.fan/nginx
