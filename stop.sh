71a164eb7967c492074a0ae9727aed8531f5cd89
fanshaohua <fanshaohua.fan@gmail.com>
2016-08-19 15:48:50 +0200

    merge conflict

--- stop.sh
+++ stop.sh
@@ -1,18 +1,10 @@
 docker stop wx
 docker stop api
 docker stop flask
-<<<<<<< HEAD
-docker stop api
-=======
->>>>>>> cfb60778dbf4397dc115fd93fc9787a781f46907
 docker stop db
 
 docker rm wx
 docker rm api
 docker rm flask
-<<<<<<< HEAD
-docker rm api
-=======
->>>>>>> cfb60778dbf4397dc115fd93fc9787a781f46907
 docker rm db
 
